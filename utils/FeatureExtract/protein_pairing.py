6c9cc0910a252f5861b95d864607e27d25793e92
gyurrikim <gyull2102@gmail.com>
2024-11-06 11:22:19 +0900

    Update protein_pairing.py

--- utils/FeatureExtract/protein_pairing.py
+++ utils/FeatureExtract/protein_pairing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import networkx as nx
 
+from copy import deepcopy
 from itertools import combinations
 
 def calc_distance(latent_features, image_labels, protein_list, pairing_protein_list):
@@ -12,7 +13,7 @@ def calc_distance(latent_features, image_labels, protein_list, pairing_protein_l
         n_data = latent_features[image_labels==image_class].shape[0]
         mean_features[protein_list[image_class]] = np.mean(latent_features[image_labels==image_class].reshape(n_data, -1), 0)
         std_features[protein_list[image_class]] = np.std(latent_features[image_labels==image_class].reshape(n_data, -1), 0)
-
+    
     distance_dictionary = dict()
     distance_matrix = np.zeros((len(pairing_protein_list),len(pairing_protein_list)))
     
@@ -20,7 +21,7 @@ def calc_distance(latent_features, image_labels, protein_list, pairing_protein_l
         ##### Project the vector #####
         subtract_vector = mean_features[protein1] - mean_features[protein2]
         subtract_vector_norm = np.linalg.norm(subtract_vector)
-
+        
         ##### feature 1 #####
         n_data1 = latent_features[image_labels==protein_list.index(protein1)].shape[0]
         features1 = latent_features[image_labels==protein_list.index(protein1)].reshape(n_data1, -1)
@@ -60,44 +61,65 @@ def calc_distance(latent_features, image_labels, protein_list, pairing_protein_l
     
     return distance_dictionary, distance_matrix
 
-def protein_pairing(G, n_pair, results=list(), used_protein=set()):
-    tmp_maximum_matching = nx.max_weight_matching(G, maxcardinality=True)
+def protein_pairing_helper(G, paired_protein, unpaired_protein):
+    G_tmp = deepcopy(G)
+    for protein in list(G_tmp.nodes):
+        if protein not in unpaired_protein:
+            G_tmp.remove_node(protein)
+
+    n_pair = len(unpaired_protein)//2
+    tmp_maximum_matching = nx.max_weight_matching(G_tmp, maxcardinality=True)
     
     if len(tmp_maximum_matching) == n_pair:
-        results.append(min([(edge, G.edges[list(edge)]["weight"]) for edge in tmp_maximum_matching], key = lambda x : x[1]))
-        used_protein.update(set(results[-1][0]))
-        
-        G.remove_node(results[-1][0][0])
-        G.remove_node(results[-1][0][1])
-        
-        if n_pair > 1:
-            results, used_protein = protein_pairing(G, n_pair-1, results, used_protein)
+        return True
+
+def protein_pairing_recursive(scores, pairing_protein_list):
+    G = nx.Graph()
+    sorted_scores = sorted(scores, key = lambda item: item[1], reverse=True)
+    
+    pairing_results = list()
+    paired_protein = list()
+    unpaired_protein = pairing_protein_list
+    n_pair = len(unpaired_protein)//2
+    
+    for score in sorted_scores:
+        if set(score[0]).issubset(set(unpaired_protein)):
+            G.add_edge(*score[0], weight = score[1])
+            
+            if len(G.edges) >= n_pair:
+                paired_protein_tmp = paired_protein + list(score[0])
+                unpaired_protein_tmp = list(set(unpaired_protein) - set(paired_protein_tmp))
+                
+                if protein_pairing_helper(G, paired_protein_tmp, unpaired_protein_tmp):
+                    pairing_results.append(score)
                     
-    return results, used_protein
+                    pairing_results_tmp, paired_, unpaired_ = protein_pairing_recursive(scores, unpaired_protein_tmp)
+                    pairing_results += pairing_results_tmp
+                    
+                    paired_protein = paired_protein_tmp + paired_
+                    unpaired_protein = unpaired_
+                    
+                    if len(unpaired_) <= 1:
+                        break
+        else:
+            continue
+    
+    return pairing_results, paired_protein, unpaired_protein
 
 def find_optimal_group_of_pairs(scores, pairing_protein_list):
     print("\n##### Spatial expression pattern guided protein pairing #####")
-    G = nx.Graph()
-    sorted_scores = sorted(scores, key = lambda item: item[1], reverse=True)
-    n_pair = len(pairing_protein_list)//2
-    for score in sorted_scores[:n_pair]:
-        G.add_edge(*score[0], weight = score[1])
-    # breakpoint()
-    for i in range(n_pair+1, len(sorted_scores)+1):
-        pair_results, used_protein = protein_pairing(G, n_pair)
-        if len(pair_results) == 0:
-            G.add_edge(*sorted_scores[i][0], weight = sorted_scores[i][1])
-        else:
-            break
-    # print the result
-    if len(pairing_protein_list) != len(used_protein):
-        unused_protein = list(set(pairing_protein_list) - set(used_protein))[0]
-        print("As the number of antibodies are odd, there is \033[93mone unused antibody\033[0m: {}".format(unused_protein))
-    print("The minimum score is \033[96m{}\033[0m".format(pair_results[0][1]))
+    
+    pairing_results, paired_protein, unpaired_protein = protein_pairing_recursive(scores, pairing_protein_list)
+    
+    if len(unpaired_protein) != 0:
+        print("As the number of proteins are odd, there is \033[93mone unused protein\033[0m: {}".format(unpaired_protein[0]))
+    
+    print("The minimum feature-based distance is \033[96m{}\033[0m".format(pairing_results[0][1]))
     print("Use the following pairs: ")
-    for pair_result in pair_results:
-        print("    ",*pair_result[0])
-    return None
+    for result in pairing_results:
+        print("    ", *result[0], "[\033[96m{}\033[0m]".format(result[1]))
+    
+    return pairing_results
 
 
 if __name__=="__main__":
